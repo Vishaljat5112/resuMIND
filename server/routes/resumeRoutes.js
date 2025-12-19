c3dfa5675e909fb66fe687442e2585c35214e783
Vishaljat5112 <153274029+Vishaljat5112@users.noreply.github.com>
2025-12-18 22:42:28 -0800

    route update

--- server/routes/resumeRoutes.js
+++ server/routes/resumeRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { uploadResume } = require("../controllers/resumeController");
 
-// 🔹 Analyze route
+
 router.post("/analyze", uploadResume);
 
-// 🔹 Test route
+
 router.get("/test", (req, res) => {
   res.send("Resume AI API Working fine with OpenRouter");
 });
